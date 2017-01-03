a0632b0a5268be21bdd4322ec424b5a8487a20a9
mladenok <mladenok@uni.coventry.ac.uk>
2017-01-03 14:44:48 +0000

    new commit

---
+++ src/subsetsum/DynamicSubsetsum.java
@@ -0,0 +1,53 @@
+package subsetsum;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author KONSTANTIN
+ * Dynamic Programming Subset-Sum Algorithm
+ * Complexity Big O Notation: O(n*t)
+ */
+public class DynamicSubsetsum {
+    static boolean isSubset(int S[], int n, int t) {
+        boolean sub[][] = new boolean[n + 1][t + 1];
+        
+        //if target is 0 -> true
+        for(int i = 0; i <= n; i++) {
+            sub[i][0] = true;
+        }
+        
+        //if target > 0 and list is empty -> false
+        for(int i = 1; i <= t; i++) {
+            sub[0][i] = false;
+        }
+        
+        //create Dynamic programming table
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= t; j++) {
+                if (j - S[i - 1] >= 0) {
+                    sub[i][j] = sub[i-1][j] || sub[i-1][j-S[i-1]]; }
+                else {
+                    sub[i][j] = sub[i-1][j];
+                }
+            }
+        }
+        
+        return sub[n][t];
+    }
+    
+    public static void main (String args[]) {
+        int S[] = {2,3,5,7,12,2,45};
+        int t = 10;
+        int n = S.length;
+        
+        if(isSubset(S, n, t) == true)
+            System.out.println("Found");
+        else
+            System.out.println("Not Found");
+    }
+}
